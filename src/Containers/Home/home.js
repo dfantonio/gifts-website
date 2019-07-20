ad8406ec4928702eb1f2ece154f01e3ffd56368a
Antônio lima <dfantonio@users.noreply.github.com>
2019-07-20 12:33:00 -0300

    Home with submitButton

--- src/Containers/Home/home.js
+++ src/Containers/Home/home.js
@@ -3,7 +3,8 @@ import "./home.css";
 
 import NavBar from "./../../Components/NavBar/NavBar";
 import CheckboxList from "./../../Components/CheckboxList/CheckboxList";
-
+import Button from "@material-ui/core/Button";
+import Footer from "./../../Components/Footer/Footer";
 const data = [
   {
     check: true,
@@ -16,22 +17,38 @@ const data = [
     name: ""
   },
   {
-    check: true,
+    check: false,
     gift: "Aranha de borracha",
     name: ""
   }
 ];
 
 class home extends Component {
+  state = {
+    presentes: []
+  };
+
   onChange = obj => {
-    console.log("home: ", obj);
+    this.setState({ presentes: [...this.state.presentes, obj] }, () => {
+      console.log("state: ", this.state.presentes);
+    });
   };
 
   render() {
     return (
-      <div>
+      <div className="root">
         <NavBar />
-        <CheckboxList data={data} onChangeXXX={() => console.log("oioioi")} />
+        <CheckboxList data={data} onChange={e => this.onChange(e)} />
+
+        <Footer>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => console.log(this.state.presentes)}
+          >
+            Submeter presente(s)
+          </Button>
+        </Footer>
       </div>
     );
   }
