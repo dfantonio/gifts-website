2362796409cbbca85555370cb087f90e43566911
Antônio lima <dfantonio@users.noreply.github.com>
2019-07-21 22:37:02 -0300

    Footer with loader integrated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
+import React from "react";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { green } from "@material-ui/core/colors";
+import Button from "@material-ui/core/Button";
+import clsx from "clsx";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,17 +20,89 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
     display: "table",
     margin: "auto"
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative"
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[700]
+    }
+  },
+  fabProgress: {
+    color: green[500],
+    position: "absolute",
+    top: -6,
+    left: -6,
+    zIndex: 1
+  },
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12
   }
 }));
 
-export default function StickyFooter({ children }) {
+export default function StickyFooter({ onClick, payload }) {
   const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const [oldPayload, setOldPayload] = React.useState(payload);
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success
+  });
+
+  React.useEffect(() => {
+    console.log("useEffect");
+    if (oldPayload.length < payload.length) setOldPayload(payload);
+    if (oldPayload.length > payload.length) {
+      console.log("zerei");
+      setSuccess(true);
+      setLoading(false);
+      setOldPayload(payload);
+    }
+  });
+
+  function handleButtonClick() {
+    if (!loading && payload) {
+      // timer.current = setTimeout(() => {
+      //   setSuccess(true);
+      //   setLoading(false);
+      // }, 2000);
+    }
+  }
+
   return (
     <div>
       <footer className={classes.footer}>
-        {/* <Container fixed maxWidth="xl"> */}
-        {children}
-        {/* </Container> */}
+        {/* {children} */}
+        <div className={classes.wrapper}>
+          <Button
+            variant="contained"
+            color="primary"
+            className={buttonClassname}
+            disabled={loading || (!payload.length && !success)}
+            onClick={() => {
+              console.log("dentro do onClick ", payload);
+              if (payload.length) {
+                setSuccess(false);
+                setLoading(true);
+                onClick();
+              }
+            }}
+          >
+            Accept terms
+          </Button>
+          {loading && (
+            <CircularProgress size={24} className={classes.buttonProgress} />
+          )}
+        </div>
       </footer>
     </div>
   );
